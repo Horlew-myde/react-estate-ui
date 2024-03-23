8ad9bf4b77af03d7b08b25a29221d797dbe5e38c
Horlew-myde <mezamune@gmail.com>
2024-03-23 21:40:25 +0100

    creating the list page

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,8 +1,17 @@
+import { listData } from '../../lib/dummyData';
 import './listPage.scss';
 
 function ListPage(){
+
+    const data = listData;
+
     return (
-        <div className='listPage'>ListPage</div>
+        <div className='listPage'>
+            <div className='listContainer'>
+                <div className='wrapper'>wrapper</div>
+            </div>
+            <div className='mapContainer'>map</div>
+        </div>
     )
 }
 
