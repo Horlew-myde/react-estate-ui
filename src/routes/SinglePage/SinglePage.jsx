9f1faff42d037f1b2c7b8902d7aad127b02e3148
Horlew-myde <mezamune@gmail.com>
2024-03-27 14:00:40 +0100

    slider

--- src/routes/SinglePage/SinglePage.jsx
+++ src/routes/SinglePage/SinglePage.jsx
@@ -1,9 +1,23 @@
-import './SinglePage.scss'
+import './SinglePage.scss';
+import Slider from "../../components/slider/slider"
 
 function SinglePage() {
     return (
-        <div className='singlePage'>SinglePage</div>
-    )
+        <div className='singlePage'>
+            <div className='details'>
+                <div className='wrapper'>
+                    <Slider/>
+                    <div className='info'>
+                        <div className='top'></div>
+                        <div className='bottom'></div>
+                    </div>
+                </div>
+            </div>
+            <div className='features'>
+                <div className='wrapper'>Wrapper</div>
+            </div>
+        </div>
+    );
 }
 
 export default SinglePage
